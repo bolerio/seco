6c7e89b9678e7087254fbe24a8887640730aa2d7
konstantin.vandev <konstantin.vandev@be6a839b-8054-0410-bcc1-119fc7438cbe>
2009-01-19 14:48:11 +0000

    git-svn-id: https://seco.googlecode.com/svn/trunk@27 be6a839b-8054-0410-bcc1-119fc7438cbe

---
+++ src/seco/notebook/OutputCellDocumentType.java
@@ -0,0 +1,34 @@
+package seco.notebook;
+
+import org.hypergraphdb.HGHandle;
+import org.hypergraphdb.HGHandleFactory;
+import org.hypergraphdb.HGPersistentHandle;
+import org.hypergraphdb.IncidenceSetRef;
+import org.hypergraphdb.LazyRef;
+import org.hypergraphdb.type.HGAtomTypeBase;
+
+
+public class OutputCellDocumentType extends HGAtomTypeBase 
+{
+    public static final HGPersistentHandle HGHANDLE = 
+        HGHandleFactory.makeHandle("4963b01e-da54-11dc-9588-d977fb5fb8c4");
+    
+    public Object make(HGPersistentHandle valueHandle, LazyRef<HGHandle[]> targetSet, IncidenceSetRef incidenceSet) 
+    {
+        HGPersistentHandle [] layout = graph.getStore().getLink(valueHandle);
+        OutputCellDocument doc = new OutputCellDocument(layout[0]); 
+        return doc;
+    }
+
+    public void release(HGPersistentHandle handle) 
+    {
+    }
+
+    public HGPersistentHandle store(Object instance) 
+    {       
+        HGPersistentHandle [] layout = new HGPersistentHandle[1];
+        OutputCellDocument doc = (OutputCellDocument)instance;
+        layout[0] =  graph.getPersistentHandle(doc.bookH);
+        return graph.getStore().store(layout);
+    }
+}
