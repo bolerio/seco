6ffa9cac75eac3f589d8bd483a701b19042852f0
borislav.iordanov <borislav.iordanov@be6a839b-8054-0410-bcc1-119fc7438cbe>
2009-03-24 16:11:05 +0000

    git-svn-id: https://seco.googlecode.com/svn/trunk@61 be6a839b-8054-0410-bcc1-119fc7438cbe

--- src/seco/events/EventDispatcher.java
+++ src/seco/events/EventDispatcher.java
@@ -1,56 +1,24 @@
 package seco.events;
 
-import java.util.Set;
-
+import java.util.List;
 import org.hypergraphdb.HGHandle;
-import org.hypergraphdb.HGHandleFactory;
 import org.hypergraphdb.HGQuery.hg;
-
 import seco.ThisNiche;
-import seco.things.CellUtils;
 
 public class EventDispatcher
 {
     public static void dispatch(HGHandle eventType, HGHandle publisher,
             Object event)
     {
-        Set<EventPubSub> set = CellUtils.findAll(ThisNiche.hg, hg.apply(hg
-                .deref(ThisNiche.hg), hg
-                .and(hg.type(EventPubSub.class), hg.incident(eventType), hg
-                        .incident(publisher), hg.orderedLink(new HGHandle[] {
-                        eventType, publisher, HGHandleFactory.anyHandle,
-                        HGHandleFactory.anyHandle }))));
-        for (EventPubSub s : set)
+        List<EventPubSub> L = hg.getAll(ThisNiche.hg,
+                                        hg.and(hg.type(EventPubSub.class), 
+                                               hg.orderedLink(eventType, publisher, hg.anyHandle(), hg.anyHandle())));
+        for (EventPubSub s : L)
         {
             EventHandler handler = (EventHandler) ThisNiche.hg.get(s
                     .getEventHandler());
             if (handler != null)
                 handler.handle(eventType, event, publisher, s.getSubscriber());
-        }
-
-        // TODO1: the above code don't work
-        if (set.isEmpty())
-        {
-            System.out.println("EventDispatcher: " + publisher + ":" + event);
-            for (HGHandle h : ThisNiche.hg.getIncidenceSet(publisher))
-            {
-                Object o = ThisNiche.hg.get(h);
-                if (o instanceof EventPubSub
-                        && ((EventPubSub) o).getEventType().equals(eventType)
-                        && ((EventPubSub) o).getPublisher().equals(publisher))
-                {
-                    System.out.println("" + o);
-                    set.add((EventPubSub) o);
-                }
-            }
-            for (EventPubSub eps : set)
-            {
-                EventHandler handler = (EventHandler) ThisNiche.hg.get(eps
-                        .getEventHandler());
-                if (handler != null)
-                    handler.handle(eventType, event, publisher, eps
-                            .getSubscriber());
-            }
         }
     }
 }
