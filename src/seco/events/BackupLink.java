33d9118784ce7a7076b1a09edb35a56096d703b7
konstantin.vandev <konstantin.vandev@be6a839b-8054-0410-bcc1-119fc7438cbe>
2009-06-17 22:27:25 +0000

    git-svn-id: https://seco.googlecode.com/svn/trunk@131 be6a839b-8054-0410-bcc1-119fc7438cbe

---
+++ src/seco/events/BackupLink.java
@@ -0,0 +1,120 @@
+package seco.events;
+
+import java.util.List;
+
+import org.hypergraphdb.HGHandle;
+import org.hypergraphdb.HGPlainLink;
+
+import seco.ThisNiche;
+
+/**
+ * <p>
+ * Represents backup info needed to restore all event handlers of a given 
+ * cell. 
+ * </p>
+ */
+public class BackupLink extends HGPlainLink 
+{
+    public BackupLink(HGHandle [] targetSet)
+    {
+        super(targetSet);
+    }
+    
+    public BackupLink(HGHandle cell, 
+                       List<EventPubSubInfo> pubs,
+                       List<EventPubSubInfo> subs)
+    {
+        super(new HGHandle[] {cell, ThisNiche.hg.add(pubs), 
+                ThisNiche.hg.add(subs)} );
+    }
+    
+    public HGHandle getCell()
+    {
+        return getTargetAt(0);
+    }
+    
+    public HGHandle getPubs()
+    {
+        return getTargetAt(1);
+    }
+    
+    public HGHandle getSubs()
+    {
+        return getTargetAt(2);
+    }
+    
+    @Override
+    public int getArity()
+    {
+       return 3;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "BackupLink:" + ThisNiche.hg.get(getCell()) + ":" +
+        ThisNiche.hg.get(getPubs()) + ":" + ThisNiche.hg.get(getSubs());
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+       if(!(obj instanceof BackupLink)) return false;
+       BackupLink in = (BackupLink) obj;
+       return getCell().equals(in.getCell()) && 
+       getPubs().equals(in.getPubs()) && 
+       getSubs().equals(in.getSubs()); 
+    }
+    
+    public static class EventPubSubInfo
+    {
+        HGHandle eventType; 
+        HGHandle pubOrSub;
+        HGHandle eventHandler;
+        
+        public EventPubSubInfo()
+        {
+            
+        }
+        
+        public EventPubSubInfo(HGHandle eventType, HGHandle pub_or_sub,
+                HGHandle eventHandler)
+        {
+            super();
+            this.eventType = eventType;
+            this.pubOrSub = pub_or_sub;
+            this.eventHandler = eventHandler;
+        }
+
+        public HGHandle getEventType()
+        {
+            return eventType;
+        }
+
+        public HGHandle getPubOrSub()
+        {
+            return pubOrSub;
+        }
+
+        public HGHandle getEventHandler()
+        {
+            return eventHandler;
+        }
+        
+        public void setEventType(HGHandle eventType)
+        {
+            this.eventType =eventType;
+        }
+
+        public void setPubOrSub(HGHandle pubOrSub)
+        {
+            this.pubOrSub = pubOrSub;
+        }
+
+        public void setEventHandler(HGHandle eventHandler)
+        {
+            this.eventHandler = eventHandler;
+        }
+    }
+
+}
