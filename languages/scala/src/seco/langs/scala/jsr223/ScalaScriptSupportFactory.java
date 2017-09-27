b056e5466d76a634353ae971fcc690d3d3d18f60
Konstantin Vandev <konstantin.vandev@gmail.com>
2017-09-27 14:42:00 +0300

    scala syntax highlighting on

--- languages/scala/src/seco/langs/scala/jsr223/ScalaScriptSupportFactory.java
+++ languages/scala/src/seco/langs/scala/jsr223/ScalaScriptSupportFactory.java
@@ -2,29 +2,29 @@ package seco.langs.scala.jsr223;
 
 import javax.swing.text.Element;
 
-import scala.tools.nsc.interpreter.Scripted;
 import seco.notebook.syntax.Mode;
 import seco.notebook.syntax.ScriptSupport;
 import seco.notebook.syntax.ScriptSupportFactory;
 
 public class ScalaScriptSupportFactory extends ScriptSupportFactory
 {
+	private static final String SCALA_NAME = "scala";
 
 	public ScalaScriptSupportFactory()
 	{
-		addMode("scala", new Mode("scala","/modes/scala.xml",this));		
+		addMode(SCALA_NAME, new Mode(SCALA_NAME,"/modes/scala.xml",this));		
 	}
 	
 	@Override
 	public String getEngineName()
 	{
-		return Scripted.ENGINE;
+		return SCALA_NAME;
 	}
 
 	@Override
 	public ScriptSupport createScriptSupport(Element el)
 	{
-		return null;
-	}
+		return new ScalaScriptSupport(this, el);
+    }
 
 }
