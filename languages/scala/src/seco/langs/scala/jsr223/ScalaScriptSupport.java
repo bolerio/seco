447f00404a4dbfe39df74b8252258c1ff11fd142
Konstantin Vandev <konstantin.vandev@gmail.com>
2017-09-29 15:34:05 +0300

    some hook point for Scala code completion

--- languages/scala/src/seco/langs/scala/jsr223/ScalaScriptSupport.java
+++ languages/scala/src/seco/langs/scala/jsr223/ScalaScriptSupport.java
@@ -2,28 +2,46 @@ package seco.langs.scala.jsr223;
 
 import javax.swing.text.Element;
 
+import seco.AppConfig;
+import seco.notebook.syntax.Formatter;
 import seco.notebook.syntax.ScriptSupport;
 import seco.notebook.syntax.ScriptSupportFactory;
 import seco.notebook.syntax.completion.CompletionProvider;
 import seco.notebook.syntax.completion.NBParser;
+import seco.notebook.syntax.java.JavaFormatter;
+import seco.notebook.syntax.java.JavaFormatterOptions;
 
 public class ScalaScriptSupport extends ScriptSupport
 {
-	public ScalaScriptSupport(ScriptSupportFactory factory, Element el)
-	{
+	private static CompletionProvider[] providers = new CompletionProvider[] { new ScalaCompletionProvider() };
+
+	public ScalaScriptSupport(ScriptSupportFactory factory, Element el) {
 		super(factory, el);
 	}
 
-	@Override
-	public CompletionProvider[] getCompletionProviders()
-	{
+	private static Formatter formatter;
+
+	public Formatter getFormatter() {
+		if (formatter == null) {
+			formatter = new JavaFormatter((JavaFormatterOptions) AppConfig.getInstance()
+					.getProperty(AppConfig.FORMATTER_OPTIONS, new JavaFormatterOptions()));
+		}
+		return formatter;
+	}
+
+	public void resetFormatter() {
+		formatter = null;
+	}
+
+	public NBParser getParser() {
+		// if(parser == null)
+		// parser = new BshAst(this);
+		// return parser;
 		return null;
 	}
 
-	@Override
-	public NBParser getParser()
-	{
-		return null;
+	public CompletionProvider[] getCompletionProviders() {
+		return providers;
 	}
 
 }
